ba665962865aff5f71e0ce7b0d3d24a763869ff4
Jake 'Sid' Smith <hello@jakesidsmith.com>
2025-02-10 15:13:44 +0000

    Disable coverage thresholds

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,14 @@ import type { Config } from 'jest';
 const config = {
   preset: 'ts-jest/presets/default-esm',
   collectCoverageFrom: ['src/**/*.(js|jsx|ts|tsx)'],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 100,
+  //     functions: 100,
+  //     lines: 100,
+  //     statements: 100,
+  //   },
+  // },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
     '(.+)\\.js$': '$1',
