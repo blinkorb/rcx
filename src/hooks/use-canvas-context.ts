d5643e748a803781524f99c57370a6e440180687
Jake 'Sid' Smith <hello@jakesidsmith.com>
2025-03-13 20:45:47 +0000

    Improve canvas context error text

--- src/hooks/use-canvas-context.ts
+++ src/hooks/use-canvas-context.ts
@@ -4,7 +4,7 @@ export const useCanvasContext = () => {
   const context = canvasContext.useInject();
 
   if (!context) {
-    throw new Error('useCanvasContext must be used inside a Canvas component');
+    throw new Error('useCanvasContext must be used below a Canvas component');
   }
 
   return context;
