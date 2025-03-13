551281ff0491f9ef9f4a4109b615f21104982721
Jake 'Sid' Smith <hello@jakesidsmith.com>
2025-03-13 13:26:53 +0000

    Make render function non-internal

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './types.ts';
-export * from './internal/index.ts';
+export * from './render.ts';
 export * from './hooks/index.ts';
 export * from './components/index.ts';
 export * from './context/index.ts';
