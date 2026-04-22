8b8cb15f43f96bd7ae491d5315165205d218fcfc
Jake 'Sid' Smith <hello@jakesidsmith.com>
2026-04-22 16:55:12 +0100

    Create rcx-vue workspace (duplicate of rcx-react with all the React bits removed)

---
+++ workspaces/eslint-config-rcx-dev/src/with-vue.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'eslint/config';
+
+import eslintConfigRCXDev from './eslint.config.js';
+
+export default defineConfig([
+  eslintConfigRCXDev,
+  // @FIXME: add vue stuff
+]);
---
+++ workspaces/rcx-vue/README.md
@@ -0,0 +1,17 @@
+# RCX Vue
+
+**Utilities for using RCX within Vue applications**
+
+## Usage
+
+### TypeScript Config With Vue
+
+FIXME
+
+### Example Usage
+
+FIXME
+
+### Linting
+
+FIXME
