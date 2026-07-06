4f62a0447ff034725f04b1820c9327fd2d3de013
Jake 'Sid' Smith <hello@jakesidsmith.com>
2026-07-07 00:42:25 +0100

    Fix assert typos

--- workspaces/rcx-2d/src/utils/assert-ctx-2d.ts
+++ workspaces/rcx-2d/src/utils/assert-ctx-2d.ts
@@ -3,11 +3,11 @@ import type {
   RCXRenderingContext,
 } from '@blinkorb/rcx';
 
-export type AssetCtx2d = (
+export type AssertCtx2d = (
   renderingContext: RCXRenderingContext
 ) => asserts renderingContext is CreateRootOptionsCtx2d;
 
-export const assertCtx2d: AssetCtx2d = (renderingContext) => {
+export const assertCtx2d: AssertCtx2d = (renderingContext) => {
   if (typeof renderingContext.ctx2d === 'undefined') {
     throw new Error(
       'A 2D component or hook was used outside of a canvas with a ctx2d (CanvasRenderingContext2D)'
--- workspaces/rcx/src/utils/assert-ctx-gl.ts
+++ workspaces/rcx/src/utils/assert-ctx-gl.ts
@@ -1,10 +1,10 @@
 import type { CreateRootOptionsCtxGl, RCXRenderingContext } from '../types.js';
 
-export type AssetCtxGl = (
+export type AssertCtxGl = (
   renderingContext: RCXRenderingContext
 ) => asserts renderingContext is CreateRootOptionsCtxGl;
 
-export const assertCtxGl: AssetCtxGl = (renderingContext) => {
+export const assertCtxGl: AssertCtxGl = (renderingContext) => {
   if (typeof renderingContext.ctxGl === 'undefined') {
     throw new Error(
       'A GL component or hook was used outside of a canvas with a ctxGl (WebGLRenderingContext)'
