be410c2109b293194205e6618cd896b3993d7567
Jake 'Sid' Smith <hello@jakesidsmith.com>
2026-07-08 01:50:18 +0100

    Add support for WebGL like tuples

--- workspaces/rcx-gl/src/internal/shader.ts
+++ workspaces/rcx-gl/src/internal/shader.ts
@@ -1,8 +1,19 @@
 import Color from 'color';
 
+import { RGBATuple, RGBTuple } from '../types.js';
+
 const MATCHES_DECIMAL = /\./g;
 
-export const colorToValidShaderVariableName = (input: string) => {
+export const colorToValidShaderVariableName = (
+  input: string | RGBTuple | RGBATuple
+) => {
+  if (Array.isArray(input)) {
+    return `rgb_${input[0] * 255}_${input[1] * 255}_${input[2] * 255}_${input[3] ?? 1}`.replace(
+      MATCHES_DECIMAL,
+      'p'
+    );
+  }
+
   const color = Color(input);
 
   return `rgb_${color.red()}_${color.green()}_${color.blue()}_${color.alpha()}`.replace(
---
+++ workspaces/rcx-gl/src/types.ts
@@ -0,0 +1,2 @@
+export type RGBATuple = [r: number, g: number, b: number, a: number];
+export type RGBTuple = [r: number, g: number, b: number];
